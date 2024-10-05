f8c77d1b114227bfd3532cbc40e7c4d7150746ae
yaongmeow <minnie_00@naver.com>
2024-10-05 16:39:13 +0900

    chore: init project

---
+++ src/main/java/org/sopt/seminar1/DiaryRepository.java
@@ -0,0 +1,10 @@
+package org.sopt.seminar1;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class DiaryRepository {
+    private final Map<Long, String> storage = new ConcurrentHashMap<>();
+    private final AtomicLong numbering = new AtomicLong();
+}
---
+++ src/main/java/org/sopt/seminar1/DiaryService.java
@@ -0,0 +1,7 @@
+package org.sopt.seminar1;
+
+import org.sopt.seminar1.DiaryRepository;
+
+public class DiaryService {
+    private final DiaryRepository diaryRepository = new DiaryRepository();
+}
