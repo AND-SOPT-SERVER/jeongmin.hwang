893c8c44ef65ddad351011748a957dc183a48fed
yaongmeow <minnie_00@naver.com>
2024-10-09 22:59:09 +0900

    fix: patch 세부 수정

--- src/main/java/org/sopt/seminar1/DiaryService.java
+++ src/main/java/org/sopt/seminar1/DiaryService.java
@@ -1,17 +1,17 @@
 package org.sopt.seminar1;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class DiaryService {
     private final DiaryRepository diaryRepository = new DiaryRepository();
-    private Map<LocalDate, Integer> patchCheck= new HashMap<>();
+    private Map<LocalDate, Integer> patchCheck = new HashMap<>();
 
-    Long savePost(String input){
-        return this.diaryRepository.savePost(input);
+    void savePost(String input){
+        this.diaryRepository.savePost(input);
     }
 
     void deletePost(Long id){
@@ -29,16 +29,20 @@ public class DiaryService {
     void updatePost(Long id, String body){
         LocalDate today = LocalDate.now();
         if (!this.patchCheck.containsKey(today)) {
-            this.patchCheck.put(today, 0);
+            this.patchCheck.put(today, 1);
         }
 
         if (this.patchCheck.get(today) > 2){
             throw new RuntimeException("patch는 하루에 2번까지만 가능합니다.");
         }
+        if (this.diaryRepository.getPost(id) == null){
+            throw new NoSuchElementException("해당 id의 post가 없습니다.");
+        }
 
         this.diaryRepository.updatePost(id, body);
         Integer patchCheck = this.patchCheck.get(today);
         this.patchCheck.put(today, patchCheck + 1);
+//        this.patchCheck.compute(today, (k, patchCheck) -> patchCheck + 1);
     }
 
     void restorePost(Long id){
